8c5a6fe0d616e2f1bb202698c5625f33feb213c7
Ahmed Osman <a.osman2024@gmail.com>
2024-10-23 20:07:10 +0100

    updated API

--- client/app.js
+++ client/app.js
@@ -1,7 +1,9 @@
 const h1 = document.querySelector("h1");
 
 async function updateh1() {
-  const response = await fetch("http://localhost:8080/message");
+  const response = await fetch(
+    "https://mono-repo-demo-server-04na.onrender.com/message"
+  );
   const data = await response.json();
   h1.textContent = data;
 }
